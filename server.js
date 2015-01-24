f4a969aed5f95dd69c13ef14f472a5e669797035
massimobiagioli <github@massimobiagioli.it>
2015-01-24 11:49:45 +0100

    Aggiunto log con morgan

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	http = require('http'),
 	cors = require('cors'),
+	morgan = require('morgan'),
 	mongoose = require('mongoose'),
 	app = express(),
+	router = express.Router(),
 	db;
 	
 // Configurazione app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined'));
 app.use(cors());
 
 // Configurazione Routes
-//app.get('/authors', routes.authors);
+router.get('/handshake', function(req, res) {
+	res.json({ message: 'handshake ok' });
+});
+app.use('/api', router);
 
 // Connessione a MongoDb
 mongoose.connect('mongodb://techbar:techbar@ds031601.mongolab.com:31601/techbar');
